428d0952acb5b2ea2c7d561aeac120c9c4538080
Raman Susla <raman.susla@sap.com>
2024-07-31 14:37:39 +0300

    add andXN and andXNasync

--- pkg/wrap/handler.go
+++ pkg/wrap/handler.go
@@ -5,22 +5,16 @@ import "errors"
 var ErrChanClosed = errors.New("channel closed")
 
 type (
-	Successor[T any, TT any] func(T) Out[TT]
+	Successor[T1, TT any]                                   func(T1) Out[TT]
+	SuccessorX2[T1, T2, TT any]                             func(T1, T2) Out[TT]
+	SuccessorX3[T1, T2, T3, TT any]                         func(T1, T2, T3) Out[TT]
+	SuccessorX4[T1, T2, T3, T4, TT any]                     func(T1, T2, T3, T4) Out[TT]
+	SuccessorX5[T1, T2, T3, T4, T5, TT any]                 func(T1, T2, T3, T4, T5) Out[TT]
+	SuccessorX6[T1, T2, T3, T4, T5, T6, TT any]             func(T1, T2, T3, T4, T5, T6) Out[TT]
+	SuccessorX7[T1, T2, T3, T4, T5, T6, T7, TT any]         func(T1, T2, T3, T4, T5, T6, T7) Out[TT]
+	SuccessorX8[T1, T2, T3, T4, T5, T6, T7, T8, TT any]     func(T1, T2, T3, T4, T5, T6, T7, T8) Out[TT]
+	SuccessorX9[T1, T2, T3, T4, T5, T6, T7, T8, T9, TT any] func(T1, T2, T3, T4, T5, T6, T7, T8, T9) Out[TT]
 )
-
-func And[T any, TT any](r Out[T], f Successor[T, TT]) Out[TT] {
-	if r.IsOK() {
-		var defaultV T
-		return f(r.GetOrDefault(defaultV))
-	}
-	return Err[TT](r.ErrorOrNil())
-}
-
-func AndAsync[T any, TT any](r Out[T], f Successor[T, TT]) Out[TT] {
-	return Async[TT](func() Out[TT] {
-		return And[T, TT](r, f)
-	})
-}
 
 func Proof(r ...ErrorContainer) Out[Empty] {
 	for _, v := range r {
@@ -176,4 +170,210 @@ func Flat[T any](r []Out[[]T]) []Out[T] {
 		res = append(res, DisJoin(v)...)
 	}
 	return res
+}
+
+func And[T any, TT any](r Out[T], f Successor[T, TT]) Out[TT] {
+	if r.IsOK() {
+		var defaultV T
+		return f(r.GetOrDefault(defaultV))
+	}
+	return Err[TT](r.ErrorOrNil())
+}
+
+func AndAsync[T any, TT any](r Out[T], f Successor[T, TT]) Out[TT] {
+	return Async[TT](func() Out[TT] {
+		return And[T, TT](r, f)
+	})
+}
+
+func AndX2[T1, T2, TT any](r1 Out[T1], r2 Out[T2], f SuccessorX2[T1, T2, TT]) Out[TT] {
+	allGood := Proof(r1, r2)
+	return And(allGood, func(Empty) Out[TT] {
+		var defaultV1 T1
+		var defaultV2 T2
+		return f(r1.GetOrDefault(defaultV1),
+			r2.GetOrDefault(defaultV2),
+		)
+	})
+}
+
+func AndX2Async[T1, T2, TT any](r1 Out[T1], r2 Out[T2], f SuccessorX2[T1, T2, TT]) Out[TT] {
+	return Async[TT](func() Out[TT] {
+		return AndX2(r1, r2, f)
+	})
+}
+
+func AndX3[T1, T2, T3, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], f SuccessorX3[T1, T2, T3, TT]) Out[TT] {
+	allGood := Proof(r1, r2)
+	return And(allGood, func(Empty) Out[TT] {
+		var defaultV1 T1
+		var defaultV2 T2
+		var defaultV3 T3
+		return f(r1.GetOrDefault(defaultV1),
+			r2.GetOrDefault(defaultV2),
+			r3.GetOrDefault(defaultV3),
+		)
+	})
+}
+
+func AndX3Async[T1, T2, T3, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], f SuccessorX3[T1, T2, T3, TT]) Out[TT] {
+	return Async[TT](func() Out[TT] {
+		return AndX3(r1, r2, r3, f)
+	})
+}
+
+func AndX4[T1, T2, T3, T4, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], f SuccessorX4[T1, T2, T3, T4, TT]) Out[TT] {
+	allGood := Proof(r1, r2)
+	return And(allGood, func(Empty) Out[TT] {
+		var defaultV1 T1
+		var defaultV2 T2
+		var defaultV3 T3
+		var defaultV4 T4
+		return f(r1.GetOrDefault(defaultV1),
+			r2.GetOrDefault(defaultV2),
+			r3.GetOrDefault(defaultV3),
+			r4.GetOrDefault(defaultV4),
+		)
+	})
+}
+
+func AndX4Async[T1, T2, T3, T4, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], f SuccessorX4[T1, T2, T3, T4, TT]) Out[TT] {
+	return Async[TT](func() Out[TT] {
+		return AndX4(r1, r2, r3, r4, f)
+	})
+}
+
+func AndX5[T1, T2, T3, T4, T5, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], f SuccessorX5[T1, T2, T3, T4, T5, TT]) Out[TT] {
+	allGood := Proof(r1, r2)
+	return And(allGood, func(Empty) Out[TT] {
+		var defaultV1 T1
+		var defaultV2 T2
+		var defaultV3 T3
+		var defaultV4 T4
+		var defaultV5 T5
+		return f(r1.GetOrDefault(defaultV1),
+			r2.GetOrDefault(defaultV2),
+			r3.GetOrDefault(defaultV3),
+			r4.GetOrDefault(defaultV4),
+			r5.GetOrDefault(defaultV5),
+		)
+	})
+}
+
+func AndX5Async[T1, T2, T3, T4, T5, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], f SuccessorX5[T1, T2, T3, T4, T5, TT]) Out[TT] {
+	return Async[TT](func() Out[TT] {
+		return AndX5(r1, r2, r3, r4, r5, f)
+	})
+}
+
+func AndX6[T1, T2, T3, T4, T5, T6, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], r6 Out[T6], f SuccessorX6[T1, T2, T3, T4, T5, T6, TT]) Out[TT] {
+	allGood := Proof(r1, r2)
+	return And(allGood, func(Empty) Out[TT] {
+		var defaultV1 T1
+		var defaultV2 T2
+		var defaultV3 T3
+		var defaultV4 T4
+		var defaultV5 T5
+		var defaultV6 T6
+		return f(r1.GetOrDefault(defaultV1),
+			r2.GetOrDefault(defaultV2),
+			r3.GetOrDefault(defaultV3),
+			r4.GetOrDefault(defaultV4),
+			r5.GetOrDefault(defaultV5),
+			r6.GetOrDefault(defaultV6),
+		)
+	})
+}
+
+func AndX6Async[T1, T2, T3, T4, T5, T6, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], r6 Out[T6], f SuccessorX6[T1, T2, T3, T4, T5, T6, TT]) Out[TT] {
+	return Async[TT](func() Out[TT] {
+		return AndX6(r1, r2, r3, r4, r5, r6, f)
+	})
+}
+
+func AndX7[T1, T2, T3, T4, T5, T6, T7, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], r6 Out[T6], r7 Out[T7], f SuccessorX7[T1, T2, T3, T4, T5, T6, T7, TT]) Out[TT] {
+	allGood := Proof(r1, r2)
+	return And(allGood, func(Empty) Out[TT] {
+		var defaultV1 T1
+		var defaultV2 T2
+		var defaultV3 T3
+		var defaultV4 T4
+		var defaultV5 T5
+		var defaultV6 T6
+		var defaultV7 T7
+		return f(r1.GetOrDefault(defaultV1),
+			r2.GetOrDefault(defaultV2),
+			r3.GetOrDefault(defaultV3),
+			r4.GetOrDefault(defaultV4),
+			r5.GetOrDefault(defaultV5),
+			r6.GetOrDefault(defaultV6),
+			r7.GetOrDefault(defaultV7),
+		)
+	})
+}
+
+func AndX7Async[T1, T2, T3, T4, T5, T6, T7, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], r6 Out[T6], r7 Out[T7], f SuccessorX7[T1, T2, T3, T4, T5, T6, T7, TT]) Out[TT] {
+	return Async[TT](func() Out[TT] {
+		return AndX7(r1, r2, r3, r4, r5, r6, r7, f)
+	})
+}
+
+func AndX8[T1, T2, T3, T4, T5, T6, T7, T8, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], r6 Out[T6], r7 Out[T7], r8 Out[T8], f SuccessorX8[T1, T2, T3, T4, T5, T6, T7, T8, TT]) Out[TT] {
+	allGood := Proof(r1, r2)
+	return And(allGood, func(Empty) Out[TT] {
+		var defaultV1 T1
+		var defaultV2 T2
+		var defaultV3 T3
+		var defaultV4 T4
+		var defaultV5 T5
+		var defaultV6 T6
+		var defaultV7 T7
+		var defaultV8 T8
+		return f(r1.GetOrDefault(defaultV1),
+			r2.GetOrDefault(defaultV2),
+			r3.GetOrDefault(defaultV3),
+			r4.GetOrDefault(defaultV4),
+			r5.GetOrDefault(defaultV5),
+			r6.GetOrDefault(defaultV6),
+			r7.GetOrDefault(defaultV7),
+			r8.GetOrDefault(defaultV8),
+		)
+	})
+}
+
+func AndX8Async[T1, T2, T3, T4, T5, T6, T7, T8, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], r6 Out[T6], r7 Out[T7], r8 Out[T8], f SuccessorX8[T1, T2, T3, T4, T5, T6, T7, T8, TT]) Out[TT] {
+	return Async[TT](func() Out[TT] {
+		return AndX8(r1, r2, r3, r4, r5, r6, r7, r8, f)
+	})
+}
+
+func AndX9[T1, T2, T3, T4, T5, T6, T7, T8, T9, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], r6 Out[T6], r7 Out[T7], r8 Out[T8], r9 Out[T9], f SuccessorX9[T1, T2, T3, T4, T5, T6, T7, T8, T9, TT]) Out[TT] {
+	allGood := Proof(r1, r2)
+	return And(allGood, func(Empty) Out[TT] {
+		var defaultV1 T1
+		var defaultV2 T2
+		var defaultV3 T3
+		var defaultV4 T4
+		var defaultV5 T5
+		var defaultV6 T6
+		var defaultV7 T7
+		var defaultV8 T8
+		var defaultV9 T9
+		return f(r1.GetOrDefault(defaultV1),
+			r2.GetOrDefault(defaultV2),
+			r3.GetOrDefault(defaultV3),
+			r4.GetOrDefault(defaultV4),
+			r5.GetOrDefault(defaultV5),
+			r6.GetOrDefault(defaultV6),
+			r7.GetOrDefault(defaultV7),
+			r8.GetOrDefault(defaultV8),
+			r9.GetOrDefault(defaultV9),
+		)
+	})
+}
+
+func AndX9Async[T1, T2, T3, T4, T5, T6, T7, T8, T9, TT any](r1 Out[T1], r2 Out[T2], r3 Out[T3], r4 Out[T4], r5 Out[T5], r6 Out[T6], r7 Out[T7], r8 Out[T8], r9 Out[T9], f SuccessorX9[T1, T2, T3, T4, T5, T6, T7, T8, T9, TT]) Out[TT] {
+	return Async[TT](func() Out[TT] {
+		return AndX9(r1, r2, r3, r4, r5, r6, r7, r8, r9, f)
+	})
 }
